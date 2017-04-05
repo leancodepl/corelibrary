022f393c0f4ca4647b6eeca08c165663bd59054b
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2017-04-05 17:05:39 +0200

    Auto-update lib version according to CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
-# Change log
+# Change Log
+All notable changes to this project will be documented in this file.
 
-## 1.0.0
-- Initial release with base packages
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## 0.1.0
+Initial release with base packages
--- build.fsx
+++ build.fsx
@@ -8,7 +8,20 @@ let srcDir = rootDir @@ "src"
 let testDir = rootDir @@ "test"
 let packDir = rootDir @@ "packed"
 
+let libVersionFile = srcDir @@ "targets/Lib.targets"
+
 let configuration = getBuildParamOrDefault "configuration" "Release"
+
+let changeLog = ChangeLogHelper.LoadChangeLog "CHANGELOG.md"
+let version = changeLog.LatestEntry.NuGetVersion
+
+let formatChangelog () =
+    let desc = defaultArg changeLog.LatestEntry.Description ""
+    let changes = System.String.Join("\n", changeLog.LatestEntry.Changes)
+    if String.IsNullOrWhiteSpace desc && String.IsNullOrWhiteSpace changes then ""
+    else if String.IsNullOrWhiteSpace desc then "Changes:\n" + changes
+    else if String.IsNullOrWhiteSpace changes then desc
+    else desc + "\n\nChanges:\n" + changes
 
 Target "Clean" (fun () ->
     !! (srcDir @@ "*/bin")
@@ -33,9 +46,27 @@ Target "Test" (fun () ->
         |> Seq.iter (fun p -> DotNetCli.Test (fun c -> { c with WorkingDir = rootDir; Project = p }))
 )
 
+Target "UpdateVersion" (fun () ->
+    XmlPokeInnerText libVersionFile "/Project/PropertyGroup/Version" version
+    XmlPokeInnerText libVersionFile "/Project/PropertyGroup/PackageReleaseNotes" (formatChangelog ())
+)
+
 Target "Pack" (fun () ->
     trace "Packaging"
     DotNetCli.Pack (fun c -> { c with WorkingDir = rootDir; Configuration = configuration; OutputPath = packDir })
+)
+
+Target "PublishToMyGet" DoNothing
+
+Target "Release" (fun () ->
+    let tagName = "v" + version
+
+    Git.Staging.StageAll ""
+    Git.Commit.Commit "" <| sprintf "Bump version to %s" version
+    Git.Branches.tag "" tagName
+
+    Git.Branches.push ""
+    Git.Branches.pushTag "" "origin" tagName
 )
 
 Target "Default" DoNothing
@@ -45,6 +76,10 @@ Target "Default" DoNothing
     ==> "Build"
     ==> "Test"
     ==> "Default"
+    ==> "UpdateVersion"
     ==> "Pack"
+    ==> "PublishToMyGet"
+
+"UpdateVersion" ==> "Release"
 
 RunTargetOrDefault "Default"
