47a8543f450e5385cc31cb815f198f4816a7b8fd
Jakub Fijałkowski <fiolek94@gmail.com>
2017-05-12 14:20:30 +0200

    Fix Razor View renderer components registration

--- src/LeanCode.ViewRenderer.Razor/RazorViewRendererModule.cs
+++ src/LeanCode.ViewRenderer.Razor/RazorViewRendererModule.cs
@@ -5,12 +5,18 @@ namespace LeanCode.ViewRenderer.Razor
 {
     class RazorViewRendererModule : Module
     {
+        private readonly RazorViewRendererOptions opts;
+
         public RazorViewRendererModule(RazorViewRendererOptions opts)
-        { }
+        {
+            this.opts = opts;
+        }
 
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<RazorViewRenderer>().As<IViewRenderer>().SingleInstance();
+            builder.Register(_ => new RazorViewRenderer(opts))
+                .As<IViewRenderer>()
+                .SingleInstance();
         }
     }
 }
