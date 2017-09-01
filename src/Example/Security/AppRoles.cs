64a5f0e4cf99a99d8c4315f590f75b0032f820f2
Sebastian Konowrocki <sebastian.konowrocki@leancode.pl>
2017-09-01 11:46:25 +0200

    Add sample authorized query to examples

---
+++ src/Example/Security/AppRoles.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using LeanCode.CQRS.Security;
+
+namespace LeanCode.Example.Security
+{
+    using Reg = Roles;
+
+    public static class Roles
+    {
+        public const string User = "leancode/user";
+        public const string Admin = "leancode/admin";
+    }
+
+    public static class Permissions
+    {
+        public const string View = "leancode/view";
+        public const string Write = "leancode/write";
+    }
+
+    public class AppRoles : IRoleRegistration
+    {
+        public IEnumerable<Role> Roles { get; } = new Role[]
+        {
+            new Role(
+                Reg.User, Permissions.View
+            ),
+            new Role(
+                Reg.Admin, Permissions.Write
+            )
+        };
+    }
+}
