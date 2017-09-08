9555c7db45ec277e311b9bc9215974994732db42
Marcin Dardzinski <mdardzinski96@gmail.com>
2017-09-08 14:50:06 +0200

    Remove unexistent namespace reference

--- src/Domain/LeanCode.DomainModels.EF/CoreUnitOfWork.cs
+++ src/Domain/LeanCode.DomainModels.EF/CoreUnitOfWork.cs
@@ -2,7 +2,6 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using LeanCode.DomainModels.DataAccess;
-using LeanCode.DomainModels.EF.Exceptions;
 using LeanCode.DomainModels.Model;
 using Microsoft.EntityFrameworkCore;
 
