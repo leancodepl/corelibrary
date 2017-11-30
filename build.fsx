5c2f06ed13e8bc7c486417af1c23e291423850ee
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2017-11-30 16:32:54 +0100

    Switch to FAKE 5

--- build.fsx
+++ build.fsx
@@ -1,19 +1,29 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 
 open System
-open Fake
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.Core.Globbing.Operators
+open Fake.IO
+open Fake.DotNet
+open Fake.DotNet.Cli
+open Fake.EnvironmentHelper
+open Fake.Tools
+open Fake.ChangeLogHelper
+open Fake.FileHelper
+open Fake.ProcessHelper
 
-let rootDir = "." |> FullName
-let srcDir = rootDir @@ "src"
-let testDir = rootDir @@ "test"
-let packDir = rootDir @@ "packed"
+let rootDir = Path.getFullName "."
+let srcDir = Path.combine rootDir "src"
+let testDir = Path.combine rootDir "test"
+let packDir = Path.combine rootDir "packed"
 
-let libVersionFile = srcDir @@ "targets/Version.targets"
-let dependenciesFile = srcDir @@ "Core/LeanCode.Targets/Dependencies.props"
+let libVersionFile = Path.combine srcDir "targets/Version.targets"
+let dependenciesFile = Path.combine srcDir "Core/LeanCode.Targets/Dependencies.props"
 
-let configuration = getBuildParamOrDefault "configuration" "Release"
+let configuration = environVarOrDefault "DOTNET_CONFIGURATION" "Release" |> BuildConfiguration.Custom
 
-let formatChangelog (changeLog: ChangeLogHelper.ChangeLog) =
+let formatChangelog (changeLog: ChangeLog) =
     let desc = defaultArg changeLog.LatestEntry.Description ""
     let changes = System.String.Join("\n", changeLog.LatestEntry.Changes)
     if String.IsNullOrWhiteSpace desc && String.IsNullOrWhiteSpace changes then ""
@@ -21,7 +31,7 @@ let formatChangelog (changeLog: ChangeLogHelper.ChangeLog) =
     else if String.IsNullOrWhiteSpace changes then desc
     else desc + "\n\nChanges:\n" + changes
 
-let updateChangelog (changeLog: ChangeLogHelper.ChangeLog) =
+let updateChangelog (changeLog: ChangeLog) =
     let branch = Git.Information.getBranchName ""
     if branch = "master" then changeLog
     else
@@ -31,55 +41,69 @@ let updateChangelog (changeLog: ChangeLogHelper.ChangeLog) =
         if Option.isSome changeLog.Unreleased
         then changeLog.PromoteUnreleased newVerString
         else
-            let newEntry = ChangeLogHelper.ChangeLogEntry.New(changeLog.LatestEntry.AssemblyVersion, newVerString, [])
+            let newEntry = ChangeLogEntry.New(changeLog.LatestEntry.AssemblyVersion, newVerString, [])
             { changeLog with Entries = newEntry :: changeLog.Entries }
 
-let changeLog = ChangeLogHelper.LoadChangeLog "CHANGELOG.md" |> updateChangelog
+let changeLog = LoadChangeLog "CHANGELOG.md" |> updateChangelog
 let version = changeLog.LatestEntry.NuGetVersion
 
-Target "Clean" (fun () ->
+DefaultDotnetCliDir <-
+    let name = "dotnet"
+    let fileName = if isUnix then name else name + ".exe"
+    Path.getDirectory <| defaultArg (tryFindFileOnPath fileName) name
+
+Target.Create "Clean" (fun _ ->
     !! (srcDir @@ "*/bin")
     ++ (srcDir @@ "*/obj")
     ++ packDir
-        |> CleanDirs
+    |> CleanDirs
 )
 
-
-Target "CleanDeploy" (fun () ->
+Target.Create "CleanDeploy" (fun _ ->
     CleanDirs [packDir]
 )
 
-Target "Restore" (fun () ->
-    trace "Restoring packages"
-    DotNetCli.Restore (fun c -> { c with WorkingDir = rootDir })
+Target.Create "Restore" (fun _ ->
+    Trace.trace "Restoring packages"
+    DotnetRestore id ""
 )
 
-Target "Build" (fun () ->
-    trace "Building packages"
-    DotNetCli.Build (fun c -> { c with WorkingDir = rootDir; Configuration = configuration })
+Target.Create "Build" (fun _ ->
+    Trace.trace "Building packages"
+    DotnetCompile (fun c ->
+        { c with
+            Configuration = configuration
+            Common =
+                { DotnetOptions.Default with
+                   CustomParams = Some "--no-restore" }
+        }) ""
 )
 
-Target "Test" (fun () ->
-    trace "Testing"
+Target.Create "Test" (fun _ ->
+    Trace.trace "Testing"
     !! (testDir @@ "**/*.csproj")
-        |> Seq.map DirectoryName
-        |> Seq.iter (fun p ->
-            DotNetCli.RunCommand (fun c -> { c with WorkingDir = p }) "xunit")
+    |> Seq.map Path.getDirectory
+    |> Seq.iter (fun p ->
+        Dotnet { DotnetOptions.Default with WorkingDirectory = p } "xunit" |> ignore)
 )
 
-Target "UpdateVersion" (fun () ->
-    XmlPokeInnerText libVersionFile "/Project/PropertyGroup/Version" version
-    XmlPokeInnerText dependenciesFile "/Project/PropertyGroup/CoreLibVersion" version
-    XmlPokeInnerText libVersionFile "/Project/PropertyGroup/PackageReleaseNotes" (formatChangelog changeLog)
+Target.Create "UpdateVersion" (fun _ ->
+    Xml.PokeInnerText libVersionFile "/Project/PropertyGroup/Version" version
+    Xml.PokeInnerText dependenciesFile "/Project/PropertyGroup/CoreLibVersion" version
+    Xml.PokeInnerText libVersionFile "/Project/PropertyGroup/PackageReleaseNotes" (formatChangelog changeLog)
 )
 
-Target "Pack" (fun () ->
-    trace "Packaging"
-    DotNetCli.Pack (fun c -> { c with WorkingDir = rootDir; Configuration = configuration; OutputPath = packDir })
+Target.Create "Pack" (fun _ ->
+    Trace.trace "Packaging"
+    DotnetPack (fun c ->
+        { c with
+            Configuration = configuration
+            OutputPath = Some packDir})
+        rootDir
 )
 
-Target "PublishToMyGet" (fun () ->
-    trace "Publishing NuGets..."
+Target.Create "PublishToMyGet" (fun _ ->
+    Trace.trace "Publishing NuGets..."
     Paket.Push (fun cfg ->
         { cfg with
             WorkingDir = packDir
@@ -88,21 +112,7 @@ Target "PublishToMyGet" (fun () ->
         })
 )
 
-Target "Release" (fun () ->
-    let branch = Git.Information.getBranchName ""
-    if branch <> "master" then failwith "You can make a release only from master branch"
-
-    let tagName = "v" + version
-
-    Git.Staging.StageAll ""
-    Git.Commit.Commit "" <| sprintf "Bump version to %s" version
-    Git.Branches.tag "" tagName
-
-    Git.Branches.push ""
-    Git.Branches.pushTag "" "origin" tagName
-)
-
-Target "Default" DoNothing
+Target.Create "Default" Target.DoNothing
 
 "CleanDeploy"
     ==> "Restore"
@@ -114,6 +124,5 @@ Target "Default" DoNothing
 
 "UpdateVersion" ?=> "Restore"
 "UpdateVersion" ==> "Pack"
-"UpdateVersion" ==> "Release"
 
-RunTargetOrDefault "Default"
+Target.RunOrDefault "Default"
