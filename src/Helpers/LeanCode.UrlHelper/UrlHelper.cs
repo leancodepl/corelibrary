1386f938e4b8016009e41d05ae1c1a9e3579e059
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2018-02-07 15:15:14 +0100

    Add UrlHelper

---
+++ src/Helpers/LeanCode.UrlHelper/UrlHelper.cs
@@ -0,0 +1,10 @@
+namespace LeanCode
+{
+    public static class UrlHelper
+    {
+        public static string Concat(string a, string b)
+        {
+            return a.TrimEnd('/') + '/' + b.TrimStart('/');
+        }
+    }
+}
