5a2026941d4bd74497163a39ef55a214c7dec94d
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2018-07-19 18:51:39 +0200

    Add pipelines benchmarks

---
+++ test/LeanCode.Benchmarks/PipelineObjects.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using LeanCode.Pipelines;
+
+namespace LeanCode.Benchmarks.Pipelines
+{
+    public struct Context : IPipelineContext
+    {
+        public IPipelineScope Scope { get; set; }
+    }
+
+    public struct Input { }
+    public struct Output { }
+
+    public class Finalizer : IPipelineFinalizer<Context, Input, Output>
+    {
+        private readonly Task<Output> Result = Task.FromResult(new Output());
+        public Task<Output> ExecuteAsync(Context ctx, Input input) => Result;
+    }
+
+    public class PassthroughElement : IPipelineElement<Context, Input, Output>
+    {
+        public Task<Output> ExecuteAsync(
+            Context ctx,
+            Input input,
+            Func<Context, Input, Task<Output>> next)
+        {
+            return next(ctx, input);
+        }
+    }
+
+    public class StaticScope : IPipelineScope
+    {
+        private readonly PassthroughElement element = new PassthroughElement();
+        private readonly Finalizer finalizer = new Finalizer();
+
+        public void Dispose() { }
+
+        public IPipelineElement<TContext, TInput, TOutput> ResolveElement<TContext, TInput, TOutput>(Type type)
+            where TContext : IPipelineContext
+        {
+            return (IPipelineElement<TContext, TInput, TOutput>)(object)element;
+        }
+
+        public IPipelineFinalizer<TContext, TInput, TOutput> ResolveFinalizer<TContext, TInput, TOutput>(Type type)
+            where TContext : IPipelineContext
+        {
+            return (IPipelineFinalizer<TContext, TInput, TOutput>)(object)finalizer;
+        }
+    }
+
+    public class StaticFactory : IPipelineFactory
+    {
+        private readonly StaticScope scope = new StaticScope();
+        public IPipelineScope BeginScope() => scope;
+    }
+}
