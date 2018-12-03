c540772c4111c12286561bcaf3a1565e69ba5f84
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2018-12-03 11:36:37 +0100

    Fix for missing `Glob` namespace

--- src/Tools/LeanCode.GlobalContractsGenerator/ContractsCompiler.cs
+++ src/Tools/LeanCode.GlobalContractsGenerator/ContractsCompiler.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using Glob;
 using LeanCode.CQRS;
 using LeanCode.CQRS.Security;
 using Microsoft.CodeAnalysis;
