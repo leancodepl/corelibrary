02a9c7460439aa7b835053f50af7cd6808aff202
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2018-12-13 10:38:23 +0100

    Add custom logic for detecting backing fields for IOptimisticConcurrency
    With EFCore 2.2, it is unable to find backing field when explicitly
    implementing the interface. Therefore, we need to add some conventions
    on our side. :(

--- src/Domain/LeanCode.DomainModels.EF/ModelBuilderExtensions.cs
+++ src/Domain/LeanCode.DomainModels.EF/ModelBuilderExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using LeanCode.DomainModels.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -6,6 +8,8 @@ namespace LeanCode.DomainModels.EF
 {
     public static class ModelBuilderExtensions
     {
+        private static readonly Type IOptimisticConcurrencyType = typeof(IOptimisticConcurrency);
+
         public static void EnableSoftDeleteOf<TSoftDeletable>(this ModelBuilder builder)
             where TSoftDeletable : class, ISoftDeletable
         {
@@ -18,7 +22,9 @@ namespace LeanCode.DomainModels.EF
             cfg.HasQueryFilter(e => e.IsDeleted == false);
         }
 
-        public static void IsOptimisticConcurrent<TEntity>(this EntityTypeBuilder<TEntity> cfg)
+        public static void IsOptimisticConcurrent<TEntity>(
+            this EntityTypeBuilder<TEntity> cfg,
+            bool useExplicitBackingFields = true)
             where TEntity : class, IOptimisticConcurrency
         {
             cfg.Property(e => e.RowVersion)
@@ -28,12 +34,44 @@ namespace LeanCode.DomainModels.EF
 
             cfg.Property(e => e.DateModified)
                 .HasColumnName(nameof(IOptimisticConcurrency.DateModified));
+
+            if (useExplicitBackingFields)
+            {
+                cfg.Property(e => e.RowVersion)
+                    .HasField(GetBackingFieldFor<TEntity>(nameof(IOptimisticConcurrency.RowVersion)));
+
+                cfg.Property(e => e.DateModified)
+                    .HasField(GetBackingFieldFor<TEntity>(nameof(IOptimisticConcurrency.DateModified)));
+            }
         }
 
-        public static void EnableOptimisticConcurrency<TEntity>(this ModelBuilder builder)
+        public static void EnableOptimisticConcurrency<TEntity>(
+            this ModelBuilder builder,
+            bool useExplicitBackingFields = true)
             where TEntity : class, IOptimisticConcurrency
         {
-            builder.Entity<TEntity>().IsOptimisticConcurrent();
+            builder.Entity<TEntity>().IsOptimisticConcurrent(useExplicitBackingFields);
+        }
+
+        private static string GetBackingFieldFor<TEntity>(string fieldName)
+        {
+            return
+                GetBackingFieldWhenAvailable<TEntity>($"<{IOptimisticConcurrencyType.FullName}.{fieldName}>k__BackingField") ??
+                GetBackingFieldWhenAvailable<TEntity>($"<{fieldName}>k__BackingField") ??
+                throw new InvalidOperationException($"Cannot find explicitly named backing field for IOptimisticConcurrency on type {typeof(TEntity).Name}.");
+        }
+
+        private static string GetBackingFieldWhenAvailable<TEntity>(string fieldName)
+        {
+            var flags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.IgnoreCase;
+            if (typeof(TEntity).GetField(fieldName, flags) is FieldInfo fi)
+            {
+                return fi.Name;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
---
+++ test/Domain/LeanCode.DomainModels.EF.Tests/ModelBuilderExtensionsTests.OptimisticConcurrency.cs
@@ -0,0 +1,144 @@
+using System;
+using LeanCode.DomainModels.Model;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace LeanCode.DomainModels.EF.Tests
+{
+    public class ModelBuilderExtensionsTests_OptimisticConcurrency
+    {
+        [Fact]
+        public void When_IOC_is_implemented_explicitly_RowVersion_has_correctly_configured_backing_field()
+        {
+            var db = new ExplicitlyContext();
+            var ent = db.Model.FindEntityType(typeof(ExplicitlyImplemented).FullName);
+            var prop = ent.FindProperty("RowVersion");
+            Assert.Equal("<LeanCode.DomainModels.Model.IOptimisticConcurrency.RowVersion>k__BackingField", prop.FieldInfo.Name);
+        }
+
+        [Fact]
+        public void When_IOC_is_implemented_explicitly_DateModified_has_correctly_configured_backing_field()
+        {
+            var db = new ExplicitlyContext();
+            var ent = db.Model.FindEntityType(typeof(ExplicitlyImplemented).FullName);
+            var prop = ent.FindProperty("DateModified");
+            Assert.Equal("<LeanCode.DomainModels.Model.IOptimisticConcurrency.DateModified>k__BackingField", prop.FieldInfo.Name);
+        }
+
+        [Fact]
+        public void When_IOC_is_implemented_implicitly_RowVersion_has_correctly_configured_backing_field()
+        {
+            var db = new ImplicitlyContext();
+            var ent = db.Model.FindEntityType(typeof(ImplicitlyImplemented).FullName);
+            var prop = ent.FindProperty("RowVersion");
+            Assert.Equal("<RowVersion>k__BackingField", prop.FieldInfo.Name);
+        }
+
+        [Fact]
+        public void When_IOC_is_implemented_implicitly_DateModified_has_correctly_configured_backing_field()
+        {
+            var db = new ImplicitlyContext();
+            var ent = db.Model.FindEntityType(typeof(ImplicitlyImplemented).FullName);
+            var prop = ent.FindProperty("DateModified");
+            Assert.Equal("<DateModified>k__BackingField", prop.FieldInfo.Name);
+        }
+
+        [Fact]
+        public void When_RowVersion_is_implemented_differently_Fails()
+        {
+            var db = new WrongRowVersionContext();
+            Assert.Throws<InvalidOperationException>(() => db.Model.FindEntityType(typeof(WrongRowVersion).FullName));
+        }
+
+        [Fact]
+        public void When_DateModified_is_implemented_differently_Fails()
+        {
+            var db = new WrongDateModifiedContext();
+            Assert.Throws<InvalidOperationException>(() => db.Model.FindEntityType(typeof(WrongDateModified).FullName));
+        }
+    }
+
+    public class ExplicitlyImplemented : IOptimisticConcurrency
+    {
+        public int Id { get; set; }
+        byte[] IOptimisticConcurrency.RowVersion { get; set; }
+        DateTime IOptimisticConcurrency.DateModified { get; set; }
+    }
+
+    public class ExplicitlyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString("N"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.EnableOptimisticConcurrency<ExplicitlyImplemented>();
+        }
+    }
+
+    public class ImplicitlyImplemented : IOptimisticConcurrency
+    {
+        public int Id { get; set; }
+        public byte[] RowVersion { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+
+    public class ImplicitlyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString("N"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.EnableOptimisticConcurrency<ImplicitlyImplemented>();
+        }
+    }
+
+    public class WrongRowVersion : IOptimisticConcurrency
+    {
+        private byte[] rowVersion;
+
+        public int Id { get; set; }
+        public byte[] RowVersion { get => rowVersion; set => rowVersion = value; }
+        public DateTime DateModified { get; set; }
+    }
+
+    public class WrongRowVersionContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString("N"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.EnableOptimisticConcurrency<WrongRowVersion>();
+        }
+    }
+
+    public class WrongDateModified : IOptimisticConcurrency
+    {
+        private DateTime dateModified;
+
+        public int Id { get; set; }
+        public byte[] RowVersion { get; set; }
+        public DateTime DateModified { get => dateModified; set => dateModified = value; }
+    }
+
+    public class WrongDateModifiedContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString("N"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.EnableOptimisticConcurrency<WrongDateModified>();
+        }
+    }
+}
