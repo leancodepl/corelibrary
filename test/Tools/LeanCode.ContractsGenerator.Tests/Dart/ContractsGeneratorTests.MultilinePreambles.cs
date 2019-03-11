d7d07a32a6f2bbfbe5b8eaf861c54fd07214a885
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2019-03-11 11:59:11 +0100

    Add multiline preamble tests

---
+++ test/Tools/LeanCode.ContractsGenerator.Tests/Dart/ContractsGeneratorTests.MultilinePreambles.cs
@@ -0,0 +1,66 @@
+using LeanCode.ContractsGenerator.Languages.Dart;
+using Xunit;
+
+namespace LeanCode.ContractsGenerator.Tests.Dart
+{
+    public partial class ContractsGeneratorTests
+    {
+        [Fact]
+        public void Empty_configuration_contains_default_preamble()
+        {
+            var configuration = GeneratorConfiguration.DeserializeConfigurationsFromString(@"
+                [
+                    {
+                        Dart: { }
+                    }
+                ]")[0].Dart;
+
+            var defaultConfiguration = new DartConfiguration();
+
+            Assert.Equal(defaultConfiguration.ContractsPreambleLines,
+                configuration.ContractsPreambleLines);
+
+            Assert.Equal(defaultConfiguration.ContractsPreamble,
+                configuration.ContractsPreamble);
+        }
+
+        [Fact]
+        public void Configuration_with_preamble_lines_is_correctly_interpreted()
+        {
+            var configuration = GeneratorConfiguration.DeserializeConfigurationsFromString(@"
+                [
+                    {
+                        Dart: {
+                            ContractsPreambleLines: [
+                                ""ContractsFirstLine"",
+                                ""ContractsSecondLine"",
+                                ""ContractsThirdLine"",
+                                """"
+                            ]
+                        }
+                    }
+                ]")[0].Dart;
+
+            var expectedContractsPreamble = "ContractsFirstLine\nContractsSecondLine\nContractsThirdLine\n";
+
+            Assert.Equal(expectedContractsPreamble, configuration.ContractsPreamble);
+        }
+
+        [Fact]
+        public void Configuration_with_joined_preamble_is_correctly_interpreted()
+        {
+            var configuration = GeneratorConfiguration.DeserializeConfigurationsFromString(@"
+                [
+                    {
+                        Dart: {
+                            ContractsPreamble: ""ContractsFirstLine\nContractsSecondLine\nContractsThirdLine\n""
+                        }
+                    }
+                ]")[0].Dart;
+
+            var expectedContractsPreamble = "ContractsFirstLine\nContractsSecondLine\nContractsThirdLine\n";
+
+            Assert.Equal(expectedContractsPreamble, configuration.ContractsPreamble);
+        }
+    }
+}
