a90548b4d51760e473a132ca0896802ce5811004
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2019-04-08 10:47:11 +0200

    Restore `PersistedGrantStoreModule`

---
+++ src/Infrastructure/LeanCode.IdentityServer.PersistedGrantStore/PersistedGrantStoreModule.cs
@@ -0,0 +1,14 @@
+using Autofac;
+using IdentityServer4.Stores;
+using LeanCode.Components;
+
+namespace LeanCode.IdentityServer.PersistedGrantStore
+{
+    public class PersistedGrantStoreModule : AppModule
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PersistedGrantStore>().As<IPersistedGrantStore>();
+        }
+    }
+}
