22df133b66c4c7981f0ea3bd87ed348fe84a662d
Marcin Dardzinski <marcin.dardzinski@leancode.pl>
2019-07-17 10:20:38 +0200

    Fix erroneous typescript generation for types having nested types but no consts

---
+++ test/Tools/LeanCode.ContractsGenerator.Tests/TypeScript/ContractsGeneratorTests.NestedTypes.cs
@@ -0,0 +1,62 @@
+using Xunit;
+using static LeanCode.ContractsGenerator.Tests.TypeScript.ContractsGeneratorTestHelpers;
+
+namespace LeanCode.ContractsGenerator.Tests.TypeScript
+{
+    public partial class ContractsGeneratorTests
+    {
+        [Fact]
+        public void Generates_globals_if_type_has_consts()
+        {
+            var generator = CreateTsGeneratorFromNamespace(
+                @"
+                public class Parent
+                {
+                    public const int Const = 2;
+                }");
+
+            var client = GetClient(generator.Generate(DefaultTypeScriptConfiguration));
+
+            Assert.Contains("Parent", client);
+            Assert.Contains("Const", client);
+        }
+
+        [Fact]
+        public void Generates_globals_if_type_children_have_consts()
+        {
+            var generator = CreateTsGeneratorFromNamespace(
+                @"
+                public class Parent
+                {
+                    public class Child
+                    {
+                        public const int Const = 2;
+                    }
+                }");
+
+            var client = GetClient(generator.Generate(DefaultTypeScriptConfiguration));
+
+            Assert.Contains("Parent", client);
+            Assert.Contains("Child", client);
+            Assert.Contains("Const", client);
+        }
+
+        [Fact]
+        public void Does_not_generate_globals_if_type_has_children_without_consts()
+        {
+            var generator = CreateTsGeneratorFromNamespace(
+                @"
+                public class Parent
+                {
+                    public class Child
+                    {
+
+                    }
+                }");
+
+            var client = GetClient(generator.Generate(DefaultTypeScriptConfiguration));
+
+            Assert.DoesNotContain("Parent", client);
+        }
+    }
+}
