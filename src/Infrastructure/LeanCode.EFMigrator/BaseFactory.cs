f20ad03366ad6f08aa1da35091bcad3de33f6ce1
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2019-08-06 11:15:13 +0200

    Add injection point for additional DbContext options and use CRTP to get assembly name

---
+++ src/Infrastructure/LeanCode.EFMigrator/BaseFactory.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LeanCode.EFMigrator
+{
+    public abstract class BaseFactory<TContext, TFactory> : IDesignTimeDbContextFactory<TContext>
+        where TContext : DbContext
+        where TFactory : BaseFactory<TContext, TFactory>
+    {
+        protected virtual string AssemblyName => typeof(TFactory).Assembly.GetName().Name;
+
+        protected virtual void UseAdditionalSqlServerDbContextOptions(
+            SqlServerDbContextOptionsBuilder builder)
+        { }
+
+        public TContext CreateDbContext(string[] args)
+        {
+            return (TContext)Activator.CreateInstance(typeof(TContext),
+                new DbContextOptionsBuilder<TContext>()
+                    .UseLoggerFactory(new ServiceCollection()
+                        .AddLogging(cfg => cfg.AddConsole())
+                        .BuildServiceProvider()
+                        .GetRequiredService<ILoggerFactory>())
+                    .UseSqlServer(
+                        MigrationsConfig.GetConnectionString(),
+                        cfg => UseAdditionalSqlServerDbContextOptions(
+                            cfg.MigrationsAssembly(AssemblyName)))
+                    .Options);
+        }
+    }
+}
