e04dd0e555bd4682f6b42c213c3b26533844898f
Emil <emil.draganczuk@leancode.pl>
2019-10-22 14:18:26 +0200

    Add test that checks SmsApi error serialization

--- test/Infrastructure/LeanCode.SmsSender.Tests/SmsSenderClientTests.cs
+++ test/Infrastructure/LeanCode.SmsSender.Tests/SmsSenderClientTests.cs
@@ -2,27 +2,51 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using System.Threading.Tasks;
+using LeanCode.SmsSender.Exceptions;
 using Xunit;
 
 namespace LeanCode.SmsSender.Tests
 {
     public class SmsSenderClientTests
     {
+        private static readonly string Login = string.Empty;
+        private static readonly string Password = string.Empty;
+        private static readonly string PhoneNumber = string.Empty;
+        private static readonly string Message = "SmsSender works fine";
+
         private static readonly SmsApiConfiguration Config = new SmsApiConfiguration
         {
-            Login = string.Empty,
-            Password = string.Empty,
+            Login = Login,
+            Password = Password,
             From = string.Empty,
             FastMode = false,
             TestMode = false,
         };
 
+        private static readonly SmsApiConfiguration ConfigWithUnregisteredSender = new SmsApiConfiguration
+        {
+            Login = Login,
+            Password = Password,
+            From = Guid.NewGuid().ToString(),
+            FastMode = false,
+            TestMode = false,
+        };
+
         private readonly SmsApiClient client;
+        private readonly SmsApiClient clientWithUnregisteredSender;
 
         public SmsSenderClientTests()
         {
             client = new SmsApiClient(
                 Config,
+                new SmsApiHttpClient(
+                    new HttpClient
+                    {
+                        BaseAddress = new Uri(SmsApiClient.ApiBase),
+                    }));
+
+            clientWithUnregisteredSender = new SmsApiClient(
+                ConfigWithUnregisteredSender,
                 new SmsApiHttpClient(
                     new HttpClient
                     {
@@ -34,9 +58,20 @@ namespace LeanCode.SmsSender.Tests
         [Fact(Skip = "SmsApi credentials required")]
         public async Task Sends_sms_correctly()
         {
-            var message = "SmsSender works fine";
-            var phoneNumber = string.Empty;
-            await client.SendAsync(message, phoneNumber);
+            await client.SendAsync(Message, PhoneNumber);
+        }
+
+        [SuppressMessage("?", "xUnit1004", Justification = "Requires custom data.")]
+        [Fact(Skip = "SmsApi credentials required")]
+        public async Task Gets_correct_errors_when_sender_is_unregistered_in_the_API()
+        {
+            try
+            {
+                await clientWithUnregisteredSender.SendAsync(Message, PhoneNumber);
+            }
+            catch (ClientException)
+            {
+            }
         }
     }
 }
