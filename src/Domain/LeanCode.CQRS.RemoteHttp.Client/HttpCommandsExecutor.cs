f60eb4239c18fe3c95294ea3f2e42b8f021a632b
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2019-10-23 14:03:25 +0200

    Ditch `CommandResultConverter` in favor of manual `JsonDocument` conversion

--- src/Domain/LeanCode.CQRS.RemoteHttp.Client/HttpCommandsExecutor.cs
+++ src/Domain/LeanCode.CQRS.RemoteHttp.Client/HttpCommandsExecutor.cs
@@ -1,9 +1,11 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using LeanCode.CQRS.Validation;
 using LeanCode.Serialization;
 
 namespace LeanCode.CQRS.RemoteHttp.Client
@@ -11,7 +13,7 @@ namespace LeanCode.CQRS.RemoteHttp.Client
     public class HttpCommandsExecutor
     {
         private readonly HttpClient client;
-        private readonly JsonSerializerOptions? serializerOptions;
+        private readonly JsonSerializerOptions serializerOptions;
 
         public HttpCommandsExecutor(HttpClient client)
             : this(client, null)
@@ -23,8 +25,6 @@ namespace LeanCode.CQRS.RemoteHttp.Client
         {
             this.client = client;
             this.serializerOptions = serializerOptions ?? new JsonSerializerOptions();
-
-            this.serializerOptions.Converters.Add(new CommandResultConverter());
         }
 
         public virtual async Task<CommandResult> RunAsync(IRemoteCommand command)
@@ -37,16 +37,7 @@ namespace LeanCode.CQRS.RemoteHttp.Client
             if (response.StatusCode == HttpStatusCode.UnprocessableEntity)
             {
                 await using var responseContent = await response.Content.ReadAsStreamAsync();
-                var result = await SafeParseResponseAsync(responseContent);
-
-                if (result is null)
-                {
-                    throw new MalformedResponseException();
-                }
-                else
-                {
-                    return result;
-                }
+                return await ParseResultAsync(responseContent);
             }
             else
             {
@@ -55,11 +46,64 @@ namespace LeanCode.CQRS.RemoteHttp.Client
             }
         }
 
-        private async Task<CommandResult?> SafeParseResponseAsync(Stream responseContent)
+        private async Task<CommandResult> ParseResultAsync(Stream responseContent)
+        {
+            // `CommandResult` does not have default constructor and we want that to stay
+
+            using var document = await ParseDocumentAsync(responseContent);
+            if (document.RootElement.ValueKind != JsonValueKind.Object)
+            {
+                throw new MalformedResponseException();
+            }
+            else if (document.RootElement.TryGetProperty(nameof(CommandResult.WasSuccessful), out var wasSuccessful) &&
+                wasSuccessful.GetBoolean())
+            {
+                return CommandResult.Success;
+            }
+            else if (document.RootElement.TryGetProperty(nameof(CommandResult.ValidationErrors), out var validationErrors) &&
+                validationErrors.ValueKind == JsonValueKind.Array)
+            {
+                var errors = validationErrors.EnumerateArray().Select(ParseError).ToList();
+                return new CommandResult(errors);
+            }
+            else
+            {
+                throw new MalformedResponseException();
+            }
+        }
+
+        private ValidationError ParseError(JsonElement el)
+        {
+            if (el.TryGetProperty(nameof(ValidationError.ErrorCode), out var errCode) &&
+                el.TryGetProperty(nameof(ValidationError.ErrorMessage), out var errMsg) &&
+                el.TryGetProperty(nameof(ValidationError.PropertyName), out var propName) &&
+                errCode.ValueKind == JsonValueKind.Number &&
+                errMsg.ValueKind == JsonValueKind.String &&
+                propName.ValueKind == JsonValueKind.String)
+            {
+                return new ValidationError(
+                    propName.GetString(),
+                    errMsg.GetString(),
+                    errCode.GetInt32());
+            }
+            else
+            {
+                throw new MalformedResponseException();
+            }
+        }
+
+        private async Task<JsonDocument> ParseDocumentAsync(Stream responseContent)
         {
             try
             {
-                return await JsonSerializer.DeserializeAsync<CommandResult?>(responseContent, serializerOptions);
+                var opts = new JsonDocumentOptions
+                {
+                    AllowTrailingCommas = serializerOptions.AllowTrailingCommas,
+                    CommentHandling = serializerOptions.ReadCommentHandling,
+                    MaxDepth = serializerOptions.MaxDepth,
+                };
+                return await JsonDocument.ParseAsync(responseContent, opts);
+
             }
             catch (Exception ex)
             {
