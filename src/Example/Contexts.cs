b37992f1c8ebb74bfc971c2353234af65c3a378a
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2019-11-07 23:30:01 +0100

    Remove unused `DefaultExecutionContext`

--- src/Example/Contexts.cs
+++ src/Example/Contexts.cs
@@ -1,11 +1,36 @@
+#nullable enable
 using System;
-using LeanCode.CQRS.Default.Execution;
+using System.Collections.Generic;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using LeanCode.Pipelines;
+using LeanCode.CQRS.Security;
+using LeanCode.DomainModels.Model;
+using LeanCode.DomainModels.EventsExecution;
 
 namespace LeanCode.Example
 {
-    public sealed class AppContext : DefaultExecutionContext
+    public sealed class AppContext : IEventsContext, ISecurityContext
     {
+        private IPipelineScope? scope;
+        private ClaimsPrincipal? user;
+
+        List<IDomainEvent> IEventsContext.SavedEvents { get; set; } = new List<IDomainEvent>();
+        List<(IDomainEvent Event, Type Handler)> IEventsContext.ExecutedHandlers { get; set; } = new List<(IDomainEvent Event, Type Handler)>();
+        List<(IDomainEvent Event, Type Handler)> IEventsContext.FailedHandlers { get; set; } = new List<(IDomainEvent Event, Type Handler)>();
+
+        IPipelineScope IPipelineContext.Scope
+        {
+            get => scope ?? throw new NullReferenceException();
+            set => scope = value;
+        }
+
+        public ClaimsPrincipal User
+        {
+            get => user ?? throw new NullReferenceException();
+            set => user = value;
+        }
+
         public Guid UserId { get; }
         public string Header { get; }
 
