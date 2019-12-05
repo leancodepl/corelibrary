9231d9231b838c9b72c779e928ca427ecc456e44
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2019-12-05 09:36:27 +0100

    Remove old-style `LeanProgram` configuration & get rid of UserSecrets

--- src/Core/LeanCode.Components.Startup/LeanProgram.cs
+++ src/Core/LeanCode.Components.Startup/LeanProgram.cs
@@ -13,53 +13,6 @@ namespace LeanCode.Components.Startup
         public const string SystemLoggersEntryName = "Serilog:SystemLoggers";
 
         [Obsolete]
-        public static IWebHostBuilder BuildDefaultWebHost<TStartup>(
-            string appName,
-            bool requireEnvSettings = false,
-            bool requireBaseSettings = true,
-            TypesCatalog? destructurers = null)
-            where TStartup : class
-        {
-            return new WebHostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    var env = hostingContext.HostingEnvironment;
-
-                    config
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json", !requireBaseSettings, true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", !requireEnvSettings, true);
-
-                    if (env.IsDevelopment())
-                    {
-                        config = config.AddUserSecrets<TStartup>();
-                    }
-
-                    config.AddEnvironmentVariables();
-                })
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    Log.Logger = new LoggerConfiguration()
-                        .EnrichWithAppName(appName)
-                        .ReadFrom.Configuration(hostingContext.Configuration)
-                        .DestructureCommonObjects(destructurers?.Assemblies)
-                        .CreateLogger();
-
-                    var config = hostingContext.Configuration
-                        .GetSection(SystemLoggersEntryName);
-
-                    logging.AddConfiguration(config);
-                    logging.AddSerilog();
-                })
-                .UseDefaultServiceProvider((context, options) =>
-                {
-                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
-                })
-                .UseStartup<TStartup>();
-        }
-
-        [Obsolete]
         public static IWebHostBuilder BuildMinimalWebHost<TStartup>(
             string appName,
             Func<WebHostBuilderContext, LoggerConfiguration> configLogger,
