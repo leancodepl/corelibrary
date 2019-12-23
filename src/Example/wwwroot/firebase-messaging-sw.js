9a8efd247ecb27bee1808f054dba3106f80df723
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2019-12-23 16:50:47 +0100

    Remove the `Example` project

--- src/Example/wwwroot/firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
-
-firebase.initializeApp({
-  'messagingSenderId': '660075770784'
-});
-
-const messaging = firebase.messaging();
