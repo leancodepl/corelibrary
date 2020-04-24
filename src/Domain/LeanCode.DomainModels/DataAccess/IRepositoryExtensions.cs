d5cc480457abde59e9bf2f1d1605ab162164b9be
Lukasz Garstecki <lukasz.garstecki@leancode.pl>
2020-04-24 09:03:28 +0200

    Remove Guid specific find and ensure exists

--- src/Domain/LeanCode.DomainModels/DataAccess/IRepositoryExtensions.cs
+++ src/Domain/LeanCode.DomainModels/DataAccess/IRepositoryExtensions.cs
@@ -6,13 +6,6 @@ namespace LeanCode.DomainModels.DataAccess
 {
     public static class IRepositoryExtensions
     {
-        public static async Task<T> FindAndEnsureExistsAsync<T>(this IRepository<T> repository, Guid id)
-            where T : class, IAggregateRootWithoutOptimisticConcurrency<Guid>
-        {
-            var entity = await repository.FindAsync(id);
-            return entity ?? throw new ArgumentException($"Aggregate of type: {typeof(T).Name} with id: {id} does not exist");
-        }
-
         public static async Task<TEntity> FindAndEnsureExistsAsync<TEntity, TIdentity>(this IRepository<TEntity, TIdentity> repository, TIdentity id)
             where TEntity : class, IAggregateRootWithoutOptimisticConcurrency<TIdentity>
             where TIdentity : notnull
