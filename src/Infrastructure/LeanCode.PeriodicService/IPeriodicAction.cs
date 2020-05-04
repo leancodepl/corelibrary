77dfad73ec5276bad4328912422eb4d7a1cb1e98
Marcin Dardzinski <marcin.dardzinski@leancode.pl>
2020-05-04 15:13:33 +0200

    Add periodic hosted service

---
+++ src/Infrastructure/LeanCode.PeriodicService/IPeriodicAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cronos;
+
+namespace LeanCode.PeriodicService
+{
+    public interface IPeriodicAction
+    {
+        CronExpression When { get; }
+        bool SkipFirstExecution { get; }
+        Task ExecuteAsync(CancellationToken stoppingToken);
+    }
+}
