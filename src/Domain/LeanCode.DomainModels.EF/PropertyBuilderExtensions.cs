b5e14666c3bc5304147035361f45084ea33a9693
Marcin Dardzinski <marcin.dardzinski@leancode.pl>
2020-07-09 10:50:13 +0200

    Add EF value converter

---
+++ src/Domain/LeanCode.DomainModels.EF/PropertyBuilderExtensions.cs
@@ -0,0 +1,20 @@
+using LeanCode.DomainModels.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeanCode.DomainModels.EF
+{
+    public static class PropertyBuilderExtensions
+    {
+        public static PropertyBuilder<Id<T>> IsTypedId<T>(this PropertyBuilder<Id<T>> builder)
+            where T : class, IIdentifiable<Id<T>>
+        {
+            return builder.HasConversion(IdConverter<T>.Instance);
+        }
+
+        public static PropertyBuilder<IId<T>> IsTypedId<T>(this PropertyBuilder<IId<T>> builder)
+            where T : class, IIdentifiable<IId<T>>
+        {
+            return builder.HasConversion(IIdConverter<T>.Instance);
+        }
+    }
+}
