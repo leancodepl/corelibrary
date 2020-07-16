fbb3a4fbf1118e599fc3245af08c05d7af0072b4
Marcin Dardzinski <marcin.dardzinski@leancode.pl>
2020-07-16 14:48:33 +0200

    Remove old push notifications client

--- src/Infrastructure/LeanCode.PushNotifications/PushNotificationsConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace LeanCode.PushNotifications
-{
-    public class PushNotificationsConfiguration
-    {
-        public bool UseDataInsteadOfNotification { get; set; }
-        public string Icon { get; set; } = string.Empty;
-    }
-}
