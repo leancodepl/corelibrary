8ba845734486156cee7ee13eaaaa839a7aae257f
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2020-09-16 16:52:33 +0200

    Handle null strings in JSON converters

--- src/Core/LeanCode.Time/System.Text.Json.Serialization.Converters/JsonTimeConverter.cs
+++ src/Core/LeanCode.Time/System.Text.Json.Serialization.Converters/JsonTimeConverter.cs
@@ -4,8 +4,12 @@ namespace System.Text.Json.Serialization.Converters
 {
     public class JsonTimeConverter : JsonConverter<Time>
     {
-        public override Time Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-            Time.Parse(reader.GetString(), CultureInfo.InvariantCulture);
+        public override Time Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetString() is string s
+                ? Time.Parse(s, CultureInfo.InvariantCulture)
+                : default;
+        }
 
         public override void Write(Utf8JsonWriter writer, Time value, JsonSerializerOptions options) =>
             writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
