9e94b6bf027c8a74bccf589a2993226cb37ba5d0
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-01-11 13:56:17 +0100

    Remove unused `ToPascalCase`

--- src/Tools/LeanCode.ContractsGenerator/Extensions/StringExtensions.cs
+++ src/Tools/LeanCode.ContractsGenerator/Extensions/StringExtensions.cs
@@ -6,10 +6,5 @@ namespace LeanCode.ContractsGenerator.Extensions
         {
             return char.ToLowerInvariant(str[0]) + str[1..];
         }
-
-        public static string ToPascalCase(this string str)
-        {
-            return char.ToUpperInvariant(str[0]) + str[1..];
-        }
     }
 }
