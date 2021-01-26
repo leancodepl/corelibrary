1761e12fd07cee4ee443adeacd3d5ffeb113aefc
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-01-26 12:26:37 +0100

    Test grant validator

---
+++ test/Infrastructure/LeanCode.ExternalIdentityProviders.Tests/ExternalLoginGrantValidatorStub.cs
@@ -0,0 +1,11 @@
+namespace LeanCode.ExternalIdentityProviders.Tests
+{
+    internal class ExternalLoginGrantValidatorStub : ExternalLoginGrantValidatorBase<User, ExternalLoginStub>
+    {
+        public override string GrantType => "stub";
+
+        public ExternalLoginGrantValidatorStub(ExternalLoginStub externalLogin)
+            : base(externalLogin)
+        { }
+    }
+}
