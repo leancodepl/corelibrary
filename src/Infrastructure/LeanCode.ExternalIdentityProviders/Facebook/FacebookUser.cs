2656db1ef8ff5fe7859f3ee95ec2ff18b48737e9
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-02-26 17:13:33 +0100

    Fix profile picture retrieval for social logins

--- src/Infrastructure/LeanCode.ExternalIdentityProviders/Facebook/FacebookUser.cs
+++ src/Infrastructure/LeanCode.ExternalIdentityProviders/Facebook/FacebookUser.cs
@@ -7,14 +7,22 @@ namespace LeanCode.ExternalIdentityProviders.Facebook
         public string? FirstName { get; }
         public string? LastName { get; }
         public string Photo { get; }
+        public bool IsSilhouette { get; }
 
-        public FacebookUser(string id, string? email, string? firstName, string? lastName, string photo)
+        public FacebookUser(
+            string id,
+            string? email,
+            string? firstName,
+            string? lastName,
+            string photo,
+            bool isSilhouette)
         {
             Id = id;
             Email = email;
             FirstName = firstName;
             LastName = lastName;
             Photo = photo;
+            IsSilhouette = isSilhouette;
         }
     }
 }
