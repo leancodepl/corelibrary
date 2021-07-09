7540bddacd37e40ece00a39c926626c92f56485c
Emil Draganczuk <emil.draganczuk@leancode.pl>
2021-07-09 15:38:03 +0200

    Make URL bases configurable

--- src/Testing/LeanCode.IntegrationTestHelpers/ConfigurationOverrides.cs
+++ src/Testing/LeanCode.IntegrationTestHelpers/ConfigurationOverrides.cs
@@ -12,27 +12,41 @@ namespace LeanCode.IntegrationTestHelpers
         public const bool EnableInternalLogsDefault = false;
         public const string ConnectionStringBaseDefault = "ConnectionStrings__DatabaseBase";
         public const string ConnectionStringKeyDefault = "ConnectionStrings:Database";
+        public const string InternalBaseKeyDefault = "InternalBase";
+        public const string PublicBaseKeyDefault = "PublicBase";
 
         private readonly LogEventLevel minimumLevel;
         private readonly bool enableInternalLogs;
         private readonly string connectionStringBase;
         private readonly string connectionStringKey;
+        private readonly string internalBaseKey;
+        private readonly string publicBaseKey;
 
         public ConfigurationOverrides(
             LogEventLevel? minimumLevel = null,
             bool? enableInternalLogs = null,
             string? connectionStringBase = null,
-            string? connectionStringKey = null)
+            string? connectionStringKey = null,
+            string? internalBaseKey = null,
+            string? publicBaseKey = null)
         {
             this.minimumLevel = minimumLevel ?? MinimumLevelDefault;
             this.enableInternalLogs = enableInternalLogs ?? EnableInternalLogsDefault;
             this.connectionStringBase = connectionStringBase ?? ConnectionStringBaseDefault;
             this.connectionStringKey = connectionStringKey ?? ConnectionStringKeyDefault;
+            this.internalBaseKey = internalBaseKey ?? InternalBaseKeyDefault;
+            this.publicBaseKey = publicBaseKey ?? PublicBaseKeyDefault;
         }
 
         public IConfigurationProvider Build(IConfigurationBuilder builder)
         {
-            return new Provider(minimumLevel, enableInternalLogs, connectionStringBase, connectionStringKey);
+            return new Provider(
+                minimumLevel,
+                enableInternalLogs,
+                connectionStringBase,
+                connectionStringKey,
+                internalBaseKey,
+                publicBaseKey);
         }
 
         private class Provider : ConfigurationProvider
@@ -41,17 +55,23 @@ namespace LeanCode.IntegrationTestHelpers
             private readonly bool enableInternalLogs;
             private readonly string connectionStringBase;
             private readonly string connectionStringKey;
+            private readonly string internalBaseKey;
+            private readonly string publicBaseKey;
 
             public Provider(
                 LogEventLevel minimumLevel,
                 bool enableInternalLogs,
                 string connectionStringBase,
-                string connectionStringKey)
+                string connectionStringKey,
+                string internalBaseKey,
+                string publicBaseKey)
             {
                 this.minimumLevel = minimumLevel;
                 this.enableInternalLogs = enableInternalLogs;
                 this.connectionStringBase = connectionStringBase;
                 this.connectionStringKey = connectionStringKey;
+                this.internalBaseKey = internalBaseKey;
+                this.publicBaseKey = publicBaseKey;
             }
 
             public override void Load()
@@ -63,8 +83,8 @@ namespace LeanCode.IntegrationTestHelpers
                 Data = new Dictionary<string, string>
                 {
                     [connectionStringKey] = dbConnStr,
-                    ["InternalBase"] = "http://localhost",
-                    ["PublicBase"] = "http://localhost",
+                    [internalBaseKey] = "http://localhost",
+                    [publicBaseKey] = "http://localhost",
                     [IHostBuilderExtensions.EnableDetailedInternalLogsKey] = enableInternalLogs.ToString(),
                     [IHostBuilderExtensions.MinimumLogLevelKey] = minimumLevel.ToString(),
                 };
