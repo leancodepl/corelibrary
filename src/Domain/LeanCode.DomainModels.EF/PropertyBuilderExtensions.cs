d2968840d05600aa71264eba0e43c5f2de3115cc
Emil Dragańczuk <emil.draganczuk@leancode.com>
2021-08-24 12:46:15 +0200

    Fix typed ID property builder extensions

--- src/Domain/LeanCode.DomainModels.EF/PropertyBuilderExtensions.cs
+++ src/Domain/LeanCode.DomainModels.EF/PropertyBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using LeanCode.DomainModels.Model;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace LeanCode.DomainModels.EF
@@ -8,37 +9,45 @@ namespace LeanCode.DomainModels.EF
         public static PropertyBuilder<Id<T>> IsTypedId<T>(this PropertyBuilder<Id<T>> builder)
             where T : class, IIdentifiable<Id<T>>
         {
-            return builder.HasConversion(IdConverter<T>.Instance);
+            return builder.HasConversion(IdConverter<T>.Instance).ValueGeneratedNever();
         }
 
         public static PropertyBuilder<Id<T>?> IsTypedId<T>(this PropertyBuilder<Id<T>?> builder)
             where T : class, IIdentifiable<Id<T>>
         {
-            return builder.HasConversion(IdConverter<T>.Instance);
+            return builder.HasConversion(IdConverter<T>.Instance).ValueGeneratedNever();
         }
 
         public static PropertyBuilder<IId<T>> IsTypedId<T>(this PropertyBuilder<IId<T>> builder)
             where T : class, IIdentifiable<IId<T>>
         {
-            return builder.HasConversion(IIdConverter<T>.Instance);
+            return builder.HasConversion(IIdConverter<T>.Instance).ValueGeneratedNever();
         }
 
         public static PropertyBuilder<IId<T>?> IsTypedId<T>(this PropertyBuilder<IId<T>?> builder)
             where T : class, IIdentifiable<IId<T>>
         {
-            return builder.HasConversion(IIdConverter<T>.Instance);
+            return builder.HasConversion(IIdConverter<T>.Instance).ValueGeneratedNever();
         }
 
         public static PropertyBuilder<SId<T>> IsTypedId<T>(this PropertyBuilder<SId<T>> builder)
             where T : class, IIdentifiable<SId<T>>
         {
-            return builder.HasConversion(SIdConverter<T>.Instance);
+            return builder
+                .HasConversion(SIdConverter<T>.Instance)
+                .HasMaxLength(SId<T>.RawLength)
+                .IsFixedLength()
+                .ValueGeneratedNever();
         }
 
         public static PropertyBuilder<SId<T>?> IsTypedId<T>(this PropertyBuilder<SId<T>?> builder)
             where T : class, IIdentifiable<SId<T>>
         {
-            return builder.HasConversion(SIdConverter<T>.Instance);
+            return builder
+                .HasConversion(SIdConverter<T>.Instance)
+                .HasMaxLength(SId<T>.RawLength)
+                .IsFixedLength()
+                .ValueGeneratedNever();
         }
     }
 }
