d58e7b9c62f9c4b23a1ed01a9135a1fa67a10db9
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2021-11-04 09:27:26 +0100

    Add UserIdLogsCorrelationMiddleware

---
+++ src/Infrastructure/LeanCode.Logging/UserIdLogsCorrelationMiddleware.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+
+namespace LeanCode.Logging
+{
+    public static class UserIdLogsCorrelationMiddleware
+    {
+        public static IApplicationBuilder UseUserIdLogsCorrelation(
+            this IApplicationBuilder builder,
+            string? userIdClaim = null)
+        {
+            return builder.Use(async (httpCtx, next) =>
+            {
+                using var userId = UserId(httpCtx, userIdClaim);
+
+                await next();
+            });
+        }
+
+        private static IDisposable? UserId(HttpContext httpCtx, string? userIdClaim)
+        {
+            if (httpCtx?.User.Identity?.IsAuthenticated ?? false)
+            {
+                var userId = httpCtx.User.FindFirstValue(userIdClaim ?? "sub");
+
+                if (!string.IsNullOrWhiteSpace(userId))
+                {
+                    return LogContext.PushProperty("UserId", userId);
+                }
+            }
+
+            return null;
+        }
+    }
+}
