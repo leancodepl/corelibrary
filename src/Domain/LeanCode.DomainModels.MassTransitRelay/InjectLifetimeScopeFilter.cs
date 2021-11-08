a91a606937bcd23926e6269fc729387ce668a09b
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2021-11-08 08:50:21 +0100

    Reintroduce liferime scope injector

---
+++ src/Domain/LeanCode.DomainModels.MassTransitRelay/InjectLifetimeScopeFilter.cs
@@ -0,0 +1,38 @@
+using GreenPipes;
+using MassTransit;
+using MassTransit.Registration;
+
+namespace LeanCode.DomainModels.MassTransitRelay
+{
+    // When combining Autofac and Microsoft.Extensions.Dependency injection
+    // some DI components are not injected correctly into the message payload,
+    // so we have to fix it by ourselves.
+    public class InjectLifetimeScopeFilter<T> : IFilter<ConsumeContext<T>>
+        where T : class
+    {
+        public void Probe(ProbeContext context)
+        { }
+
+        public Task Send(ConsumeContext<T> context, IPipe<ConsumeContext<T>> next)
+        {
+            if (!context.TryGetPayload(out Autofac.Extensions.DependencyInjection.AutofacServiceProvider scopeProvider))
+            {
+                throw new InvalidOperationException("Could not find autofac service provider");
+            }
+
+            context.GetOrAddPayload(() => scopeProvider.LifetimeScope);
+            return next.Send(context);
+        }
+    }
+
+    public static class InjectLifetimeScopeFilter
+    {
+        public static void UseLifetimeScopeInjection(this IConsumePipeConfigurator configurator, IConfigurationServiceProvider ctx)
+        {
+            DependencyInjectionFilterExtensions.UseConsumeFilter(
+                configurator,
+                typeof(InjectLifetimeScopeFilter<>),
+                ctx);
+        }
+    }
+}
