f1b2cb9670043bf967fdbb1a7a28354ac38bb6d9
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2022-01-17 16:27:30 +0100

    Allow excluding fields from contracts generation

--- src/Domain/LeanCode.CQRS/ExcludeFromContractsGenerationAttribute.cs
+++ src/Domain/LeanCode.CQRS/ExcludeFromContractsGenerationAttribute.cs
@@ -4,7 +4,7 @@ using AT = System.AttributeTargets;
 namespace LeanCode.CQRS
 {
     [AttributeUsage(
-        AT.Class | AT.Struct | AT.Enum | AT.Interface | AT.Property,
+        AT.Class | AT.Struct | AT.Enum | AT.Interface | AT.Property | AT.Field,
         AllowMultiple = false, Inherited = false)]
     public sealed class ExcludeFromContractsGenerationAttribute : Attribute { }
 }
