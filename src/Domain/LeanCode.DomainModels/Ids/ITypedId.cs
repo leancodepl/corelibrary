7508fd16d5943076123c4d81fcb25615087bd34f
Jakub Fijałkowski <kuba@codinginfinity.me>
2023-02-26 14:53:19 +0100

    Minimize the `ITypedId` required API surface

--- src/Domain/LeanCode.DomainModels/Ids/ITypedId.cs
+++ src/Domain/LeanCode.DomainModels/Ids/ITypedId.cs
@@ -8,17 +8,9 @@ public interface IPrefixedTypedId<TSelf> : IEquatable<TSelf>, IComparable<TSelf>
     where TSelf : struct, IPrefixedTypedId<TSelf>
 {
     string Value { get; }
-    bool IsEmpty { get; }
-
-    public static abstract TSelf Parse(string? v);
-
-    [return: NotNullIfNotNull("id")]
-    public static abstract TSelf? ParseNullable(string? id);
-    public static abstract bool TryParse([NotNullWhen(true)] string? v, out TSelf id);
-    public static abstract bool IsValid([NotNullWhen(true)] string? v);
-
-    public static abstract System.Linq.Expressions.Expression<Func<string, TSelf>> FromDatabase { get; }
     public static abstract int RawLength { get; }
+    public static abstract TSelf Parse(string? v);
+    public static abstract System.Linq.Expressions.Expression<Func<string, TSelf>> FromDatabase { get; }
 }
 
 [SuppressMessage("?", "CA1000", Justification = "Roslyn bug.")]
@@ -28,14 +20,6 @@ public interface IRawTypedId<TBacking, TSelf> : IEquatable<TSelf>, IComparable<T
     where TSelf : struct, IRawTypedId<TBacking, TSelf>
 {
     TBacking Value { get; }
-    bool IsEmpty { get; }
-
     public static abstract TSelf Parse(TBacking? v);
-
-    [return: NotNullIfNotNull("id")]
-    public static abstract TSelf? ParseNullable(TBacking? id);
-    public static abstract bool TryParse([NotNullWhen(true)] TBacking? v, out TSelf id);
-    public static abstract bool IsValid([NotNullWhen(true)] TBacking? v);
-
     public static abstract System.Linq.Expressions.Expression<Func<TBacking, TSelf>> FromDatabase { get; }
 }
