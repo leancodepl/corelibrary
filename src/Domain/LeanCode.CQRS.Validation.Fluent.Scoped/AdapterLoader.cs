6f0e952f440a6a8f080b222ca5d613aba769dae6
Mateusz Kulak <mateusz0990@gmail.com>
2023-02-27 09:39:55 +0100

    Add Scoped Fluent Validation

---
+++ src/Domain/LeanCode.CQRS.Validation.Fluent.Scoped/AdapterLoader.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Autofac;
+using FluentValidation;
+using LeanCode.Contracts;
+using LeanCode.Contracts.Validation;
+
+namespace LeanCode.CQRS.Validation.Fluent.Scoped;
+
+internal class AdapterLoader<TAppContext, TCommand> : ICommandValidator<TAppContext, TCommand>
+    where TAppContext : notnull
+    where TCommand : ICommand
+{
+    private static readonly Task<ValidationResult> NoError = Task.FromResult(new ValidationResult(null));
+
+    private readonly FluentValidationCommandValidatorAdapter<TAppContext, TCommand>? adapter;
+
+    public AdapterLoader(IComponentContext ctx)
+    {
+        if (ctx.TryResolve<IValidator<TCommand>>(out var val))
+        {
+            adapter = new FluentValidationCommandValidatorAdapter<TAppContext, TCommand>(val, ctx);
+        }
+    }
+
+    public Task<ValidationResult> ValidateAsync(TAppContext appContext, TCommand command)
+    {
+        return adapter is null ? NoError : adapter.ValidateAsync(appContext, command);
+    }
+}
---
+++ src/Domain/LeanCode.CQRS.Validation.Fluent.Scoped/ValidationContextExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using Autofac;
+using FluentValidation;
+
+namespace LeanCode.CQRS.Validation.Fluent.Scoped;
+
+public static class ValidationContextExtensions
+{
+    public const string ComponentContextKey = "ComponentContext";
+    public const string AppContextKey = "AppContext";
+
+    public static TAppContext AppContext<TAppContext>(this IValidationContext ctx)
+        where TAppContext : class
+    {
+        if (ctx.RootContextData.TryGetValue(AppContextKey, out var ac))
+        {
+            return (TAppContext)ac;
+        }
+        else
+        {
+            throw new InvalidOperationException(
+                "Cannot use `AppContext` extension method outside the `ContextualValidator` class."
+            );
+        }
+    }
+
+    public static T GetService<T>(this IValidationContext ctx)
+        where T : class
+    {
+        if (ctx.RootContextData.TryGetValue(ComponentContextKey, out var cc))
+        {
+            return ((IComponentContext)cc).Resolve<T>();
+        }
+        else
+        {
+            throw new InvalidOperationException(
+                "Cannot use `GetService` extension method outside the `ContextualValidator` class."
+            );
+        }
+    }
+}
