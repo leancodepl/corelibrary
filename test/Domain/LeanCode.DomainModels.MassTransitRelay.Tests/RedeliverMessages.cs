9d25bdc514db8cd99ab0a7ecafe7e002683bebd2
Mateusz Kulak <mateusz0990@gmail.com>
2023-03-15 14:32:15 +0100

    Fix all warnings

--- test/Domain/LeanCode.DomainModels.MassTransitRelay.Tests/RedeliverMessages.cs
+++ test/Domain/LeanCode.DomainModels.MassTransitRelay.Tests/RedeliverMessages.cs
@@ -9,25 +9,24 @@ public class RedeliverMessages
     [Fact]
     public async Task Redelivered_messages_are_not_filtered_by_ConsumeMessageOnceFilter()
     {
-        var harness = new InMemoryTestHarness();
-
-        harness.OnConfigureInMemoryBus += configuration =>
+        using (InMemoryTestHarness harness = new InMemoryTestHarness())
         {
-            configuration.UseDelayedMessageScheduler();
-        };
+            harness.OnConfigureInMemoryBus += configuration =>
+            {
+                configuration.UseDelayedMessageScheduler();
+            };
 
-        var consumerHarness = harness.Consumer<MyConsumer>();
+            var consumerHarness = harness.Consumer<MyConsumer>();
 
-        await harness.Start();
+            await harness.Start();
 
-        try
-        {
             var message = new MyMessage { Text = "test" };
             await harness.InputQueueSendEndpoint.Send(message);
 
             Assert.True(await consumerHarness.Consumed.SelectAsync<MyMessage>().Any());
 
-            var context = consumerHarness.Consumed.Select<MyMessage>().LastOrDefault()?.Context;
+            var messages = await consumerHarness.Consumed.SelectAsync<MyMessage>().ToListAsync();
+            var context = messages.LastOrDefault()?.Context;
 
             var redeliveryCount = context.GetRedeliveryCount();
             var payload = context.GetPayload<ConsumeContext>();
@@ -38,19 +37,16 @@ public class RedeliverMessages
             await context.Redeliver(TimeSpan.FromSeconds(1));
             await Task.Delay(TimeSpan.FromSeconds(1));
 
-            context = consumerHarness.Consumed.Select<MyMessage>().LastOrDefault().Context;
+            messages = await consumerHarness.Consumed.SelectAsync<MyMessage>().ToListAsync();
+            context = messages.LastOrDefault()?.Context;
             redeliveryCount = context.GetRedeliveryCount();
 
-            Assert.Equal(2, consumerHarness.Consumed.Select<MyMessage>().Count());
+            Assert.Equal(2, await consumerHarness.Consumed.SelectAsync<MyMessage>().Count());
             Assert.Equal(1, redeliveryCount);
-        }
-        finally
-        {
-            await harness.Stop();
         }
     }
 
-    private class MyConsumer : IConsumer<MyMessage>
+    private sealed class MyConsumer : IConsumer<MyMessage>
     {
         public async Task Consume(ConsumeContext<MyMessage> context)
         {
@@ -58,7 +54,7 @@ public class RedeliverMessages
         }
     }
 
-    private class MyMessage
+    private sealed class MyMessage
     {
         public string Text { get; set; }
     }
