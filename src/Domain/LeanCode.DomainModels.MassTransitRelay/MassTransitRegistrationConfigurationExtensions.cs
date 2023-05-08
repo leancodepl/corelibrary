cba4169666b122143e112cc438669fecd6999b37
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2023-05-08 15:13:27 +0200

    Add extension to register consumers with default definition

---
+++ src/Domain/LeanCode.DomainModels.MassTransitRelay/MassTransitRegistrationConfigurationExtensions.cs
@@ -0,0 +1,57 @@
+using System.Reflection;
+using MassTransit;
+using MassTransit.Internals;
+using MassTransit.Metadata;
+using MassTransit.Util;
+
+namespace LeanCode.DomainModels.MassTransitRelay;
+
+public static class MassTransitRegistrationConfigurationExtensions
+{
+    // These methods are based on
+    // https://github.com/MassTransit/MassTransit/blob/develop/src/MassTransit/Configuration/RegistrationExtensions.cs
+    public static void AddConsumersWithDefaultConfiguration(
+        this IRegistrationConfigurator configurator,
+        IEnumerable<Assembly> assemblies,
+        Type defaultDefinition
+    )
+    {
+        var result = AssemblyTypeCache
+            .FindTypes(assemblies, RegistrationMetadata.IsConsumerOrDefinition)
+            .GetAwaiter()
+            .GetResult();
+        var types = result
+            .FindTypes(TypeClassification.Closed | TypeClassification.Concrete)
+            .ToArray();
+        configurator.AddConsumersWithDefaultConfiguration(types, defaultDefinition);
+    }
+
+    public static void AddConsumersWithDefaultConfiguration(
+        this IRegistrationConfigurator configurator,
+        Type[] types,
+        Type defaultDefinition
+    )
+    {
+        var outer = types.Where(MessageTypeCache.HasConsumerInterfaces);
+        var inner = types.Where((Type x) => x.HasInterface(typeof(IConsumerDefinition<>)));
+        var enumerable =
+            from c in outer
+            join d in inner on c equals d.GetClosingArgument(typeof(IConsumerDefinition<>)) into dc
+            from d in dc.DefaultIfEmpty()
+            select new { ConsumerType = c, DefinitionType = d };
+        foreach (var item in enumerable)
+        {
+            if (item.DefinitionType is null)
+            {
+                configurator.AddConsumer(
+                    item.ConsumerType,
+                    defaultDefinition.MakeGenericType(item.ConsumerType)
+                );
+            }
+            else
+            {
+                configurator.AddConsumer(item.ConsumerType, item.DefinitionType);
+            }
+        }
+    }
+}
