c298e5661afbcb59808e2ae9e265e40ca53f620b
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2023-05-17 11:05:53 +0200

    Migrate response logging elements to asp.net middleware

---
+++ src/Domain/LeanCode.CQRS.AspNetCore/Middleware/NonProductionResponseLoggerMiddleware.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace LeanCode.CQRS.AspNetCore.Middleware;
+
+public class NonProductionResponseLoggerMiddleware
+{
+    private readonly ILogger logger;
+    private readonly IHostEnvironment environment;
+
+    public NonProductionResponseLoggerMiddleware(IHostEnvironment env)
+    {
+        environment = env;
+        logger = Log.ForContext<NonProductionResponseLoggerMiddleware>();
+    }
+
+    public NonProductionResponseLoggerMiddleware(IHostEnvironment env, ILogger logger)
+    {
+        environment = env;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
+    {
+        await next(httpContext);
+
+        var payload = httpContext.GetCQRSRequestPayload();
+
+        if (!environment.IsProduction())
+        {
+            logger.Information("Request executed with response {@Response}", payload.Result);
+        }
+
+    }
+}
+
+public static class LogResponsesOnNonProductionPipelineBuilderExtensions
+{
+    public static IApplicationBuilder LogCQRSResponsesOnNonProduction<TContext, TObj, TRes>(
+        this IApplicationBuilder builder
+    )
+    {
+        return builder.UseMiddleware<NonProductionResponseLoggerMiddleware>();
+    }
+}
