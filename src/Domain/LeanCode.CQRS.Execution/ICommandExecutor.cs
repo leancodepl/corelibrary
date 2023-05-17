dc5158bab7d6d0c95d0e04de6eb39af8a83ed15c
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2023-05-17 11:34:12 +0200

    Remove no longer used executors/finalizers

--- src/Domain/LeanCode.CQRS.Execution/ICommandExecutor.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using LeanCode.Contracts;
-
-namespace LeanCode.CQRS.Execution;
-
-public interface ICommandExecutor<TAppContext>
-{
-    Task<CommandResult> RunAsync(TAppContext appContext, ICommand command);
-}
