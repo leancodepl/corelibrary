3e6e4746e2fb02d91b44ca4cc3cc40667dd29f70
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-06-26 16:22:14 +0200

    Define C# models for Kratos identity objects
    Credentials were omitted on purpose.

---
+++ src/Infrastructure/LeanCode.Kratos/Model/Identity.cs
@@ -0,0 +1,54 @@
+#nullable disable warnings
+#pragma warning disable CA2227 // Collection properties should be read only
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using LeanCode.Serialization;
+
+namespace LeanCode.Kratos.Model;
+
+public class Identity
+{
+    [JsonPropertyName("state")]
+    public IdentityState State { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("recovery_addresses")]
+    public List<RecoveryIdentityAddress>? RecoveryAddresses { get; set; }
+
+    [JsonPropertyName("schema_id")]
+    public string SchemaId { get; set; }
+
+    [JsonPropertyName("schema_url")]
+    public Uri SchemaUrl { get; set; }
+
+    [JsonPropertyName("state_changed_at")]
+    public DateTime? StateChangedAt { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("verifiable_addresses")]
+    public List<VerifiableIdentityAddress>? VerifiableAddresses { get; set; }
+
+    [JsonPropertyName("metadata_admin")]
+    public JsonElement? MetadataAdmin { get; set; }
+
+    [JsonPropertyName("metadata_public")]
+    public JsonElement? MetadataPublic { get; set; }
+
+    [JsonPropertyName("traits")]
+    public JsonElement Traits { get; set; }
+}
+
+[JsonConverter(typeof(JsonSnakeCaseLowerStringEnumConverter))]
+public enum IdentityState
+{
+    Active,
+    Inactive,
+}
---
+++ src/Infrastructure/LeanCode.Kratos/Model/IdentityAddress.cs
@@ -0,0 +1,66 @@
+#nullable disable warnings
+
+using System.Text.Json.Serialization;
+using LeanCode.Serialization;
+
+namespace LeanCode.Kratos.Model;
+
+public class RecoveryIdentityAddress
+{
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("value")]
+    public string Value { get; set; }
+
+    [JsonPropertyName("via")]
+    public AddressType Via { get; set; }
+}
+
+public class VerifiableIdentityAddress
+{
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("status")]
+    public AddressStatus Status { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("value")]
+    public string Value { get; set; }
+
+    [JsonPropertyName("verified")]
+    public bool Verified { get; set; }
+
+    [JsonPropertyName("verified_at")]
+    public DateTime? VerifiedAt { get; set; }
+
+    [JsonPropertyName("via")]
+    public AddressType Via { get; set; }
+}
+
+[JsonConverter(typeof(JsonSnakeCaseLowerStringEnumConverter))]
+public enum AddressType
+{
+    Email,
+    Phone,
+}
+
+[JsonConverter(typeof(JsonSnakeCaseLowerStringEnumConverter))]
+public enum AddressStatus
+{
+    Pending,
+    Sent,
+    Completed,
+}
