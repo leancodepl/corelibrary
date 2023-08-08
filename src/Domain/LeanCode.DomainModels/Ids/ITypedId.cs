beb7b1a78c4d05fbf43887db25108293fd6700bc
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2023-08-08 14:10:29 +0200

    Implement IEqualityOperators and `IsValid` for typed ids

--- src/Domain/LeanCode.DomainModels/Ids/ITypedId.cs
+++ src/Domain/LeanCode.DomainModels/Ids/ITypedId.cs
@@ -1,17 +1,24 @@
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using System.Numerics;
 
 namespace LeanCode.DomainModels.Ids;
 
 [SuppressMessage("?", "CA1000", Justification = "Roslyn bug.")]
 [EditorBrowsable(EditorBrowsableState.Never)]
-public interface IPrefixedTypedId<TSelf> : IEquatable<TSelf>, IComparable<TSelf>, ISpanFormattable, IUtf8SpanFormattable
+public interface IPrefixedTypedId<TSelf>
+    : IEquatable<TSelf>,
+        IComparable<TSelf>,
+        ISpanFormattable,
+        IUtf8SpanFormattable,
+        IEqualityOperators<TSelf, TSelf, bool>
     where TSelf : struct, IPrefixedTypedId<TSelf>
 {
     string Value { get; }
     public static abstract int RawLength { get; }
     public static abstract TSelf Parse(string? v);
+    public static abstract bool IsValid(string? v);
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static abstract Expression<Func<string, TSelf>> FromDatabase { get; }
@@ -26,7 +33,8 @@ public interface IRawTypedId<TBacking, TSelf>
     : IEquatable<TSelf>,
         IComparable<TSelf>,
         ISpanFormattable,
-        IUtf8SpanFormattable
+        IUtf8SpanFormattable,
+        IEqualityOperators<TSelf, TSelf, bool>
     where TBacking : struct
     where TSelf : struct, IRawTypedId<TBacking, TSelf>
 {
