979a8c45f4cda4d6c2f84f627788680f42beaf76
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2023-09-15 15:48:33 +0200

    Add postgres to integration tests

--- test/LeanCode.IntegrationTests/TestDatabaseConfig.cs
+++ test/LeanCode.IntegrationTests/TestDatabaseConfig.cs
@@ -1,29 +1,38 @@
 using LeanCode.CQRS.MassTransitRelay.LockProviders;
 using LeanCode.IntegrationTestHelpers;
-using LeanCode.IntegrationTests.App;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace LeanCode.IntegrationTests;
 
 public abstract class TestDatabaseConfig
 {
+    public const string ConfigEnvName = "LeanCodeIntegrationTests__Database";
+
     public abstract ConfigurationOverrides GetConfigurationOverrides();
     public abstract void ConfigureDbContext(DbContextOptionsBuilder builder, IConfiguration config);
     public abstract void ConfigureMassTransitOutbox(IEntityFrameworkOutboxConfigurator configurator);
 
     public static TestDatabaseConfig Create()
     {
-        return new MssqlTestDatabaseConfig();
+        return Environment.GetEnvironmentVariable(ConfigEnvName) switch
+        {
+            "sqlserver" => new SqlServerTestDatabaseConfig(),
+            "postgres" => new PostgresTestConfig(),
+            _
+                => throw new InvalidOperationException(
+                    $"Set the database provider (sqlserver|postgres) via {ConfigEnvName} env variable"
+                ),
+        };
     }
 }
 
-public class MssqlTestDatabaseConfig : TestDatabaseConfig
+public class SqlServerTestDatabaseConfig : TestDatabaseConfig
 {
-    public override ConfigurationOverrides GetConfigurationOverrides() => new(
-        "SqlServer__ConnectionStringBase",
-        "SqlServer:ConnectionString");
+    public override ConfigurationOverrides GetConfigurationOverrides() =>
+        new("SqlServer__ConnectionStringBase", "SqlServer:ConnectionString");
 
     public override void ConfigureDbContext(DbContextOptionsBuilder builder, IConfiguration config)
     {
@@ -36,3 +45,19 @@ public class MssqlTestDatabaseConfig : TestDatabaseConfig
     }
 }
 
+public class PostgresTestConfig : TestDatabaseConfig
+{
+    public override ConfigurationOverrides GetConfigurationOverrides() =>
+        new("Postgres__ConnectionStringBase", "Postgres:ConnectionString");
+
+    public override void ConfigureDbContext(DbContextOptionsBuilder builder, IConfiguration config)
+    {
+        var dataSource = new NpgsqlDataSourceBuilder(config.GetValue<string>("Postgres:ConnectionString")).Build();
+        builder.UseNpgsql(dataSource);
+    }
+
+    public override void ConfigureMassTransitOutbox(IEntityFrameworkOutboxConfigurator configurator)
+    {
+        configurator.LockStatementProvider = new CustomPostgresLockStatementProvider();
+    }
+}
