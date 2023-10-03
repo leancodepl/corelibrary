485d4daab4941d8c3df90ccaff668290299ceb9e
Jakub Fijałkowski <kuba@codinginfinity.me>
2023-10-03 13:59:01 +0200

    Implement `EFRepository.FindAsync` in terms of `BaseQuery` and default `FirstOrDefaultAsync` call

--- src/Domain/LeanCode.DomainModels.EF/EFRepository.cs
+++ src/Domain/LeanCode.DomainModels.EF/EFRepository.cs
@@ -60,7 +60,19 @@ public abstract class EFRepository<TEntity, TIdentity, TContext> : IRepository<T
         }
     }
 
-    public abstract Task<TEntity?> FindAsync(TIdentity id, CancellationToken cancellationToken = default);
+    protected virtual IQueryable<TEntity> BaseQuery() => DbSet.AsQueryable();
+
+    /// <summary>
+    /// Finds an entity by primary key.
+    /// </summary>
+    /// <remarks>For implementers: the default implementation won't work for composite primary keys.</remarks>
+    /// <param name="id">The identifier that</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete.</param>
+    /// <returns>Task with the found entity, or -null- if not found.</returns>
+    public virtual async Task<TEntity?> FindAsync(TIdentity id, CancellationToken cancellationToken = default)
+    {
+        return await BaseQuery().AsTracking().FirstOrDefaultAsync(e => e.Id.Equals(id), cancellationToken);
+    }
 
     [SuppressMessage(
         "?",
