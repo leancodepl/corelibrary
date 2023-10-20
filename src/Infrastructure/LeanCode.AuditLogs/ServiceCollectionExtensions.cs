0a26b6361ed10fbfe5d14711e62774ce32233787
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2023-10-20 10:46:09 +0200

    Remove generic audit logs configuration

--- src/Infrastructure/LeanCode.AuditLogs/ServiceCollectionExtensions.cs
+++ src/Infrastructure/LeanCode.AuditLogs/ServiceCollectionExtensions.cs
@@ -4,14 +4,6 @@ namespace LeanCode.AuditLogs;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddAuditLogs<TAuditLogStorage>(this IServiceCollection services)
-        where TAuditLogStorage : class, IAuditLogStorage
-    {
-        services.AddTransient<AuditLogsPublisher>();
-        services.AddTransient<IAuditLogStorage, TAuditLogStorage>();
-        return services;
-    }
-
     public static IServiceCollection AddAzureStorageAuditLogs(
         this IServiceCollection services,
         AzureBlobAuditLogStorageConfiguration config
