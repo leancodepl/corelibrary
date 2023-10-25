dd17c04559c92848a846ebb35b45fdaebd83a61c
Wojciech Klusek <wojciech.klusek@leancode.pl>
2023-10-25 09:32:43 +0200

    Add CancellationToken convention analyzer

---
+++ src/Tools/LeanCode.CodeAnalysis/Analyzers/CancellationTokensShouldFollowNamingConvention.cs
@@ -0,0 +1,66 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace LeanCode.CodeAnalysis.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class CancellationTokensShouldFollowNamingConvention : DiagnosticAnalyzer
+{
+    internal const string IdentifierName = "CancellationToken";
+    internal const string ExpectedName = "cancellationToken";
+
+    private static readonly DiagnosticDescriptor Rule =
+        new(
+            DiagnosticsIds.CancellationTokensShouldFollowNamingConvention,
+            "CancellationTokens should follow naming convention",
+            "`{0}` should follow `{1}` naming convention",
+            "Naming",
+            DiagnosticSeverity.Warning,
+            true
+        );
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(
+            GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics
+        );
+        context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
+    }
+
+    private static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
+    {
+        // Skip analyzing methods with 'override' or 'new' keywords as they might come from external source
+        if (
+            context.Node is MethodDeclarationSyntax methodDeclaration
+            && !methodDeclaration.Modifiers.Any(
+                modifier => modifier.IsKind(SyntaxKind.OverrideKeyword) || modifier.IsKind(SyntaxKind.NewKeyword)
+            )
+        )
+        {
+            foreach (var parameter in methodDeclaration.ParameterList.Parameters)
+            {
+                if (
+                    parameter.Type is IdentifierNameSyntax identifierName
+                    && identifierName.Identifier.Text == IdentifierName
+                    && parameter.Identifier.Text != ExpectedName
+                )
+                {
+                    var diagnostic = Diagnostic.Create(
+                        Rule,
+                        parameter.Identifier.GetLocation(),
+                        parameter.Identifier.Text,
+                        ExpectedName
+                    );
+
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
