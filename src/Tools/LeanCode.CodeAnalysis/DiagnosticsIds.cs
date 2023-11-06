505a54427ef3d0ec78c47381b1c76977f4f28a13
Wojciech Klusek <wojciech.klusek@leancode.pl>
2023-11-06 11:43:06 +0100

    Add CQRS handlers naming convention analyzer

--- src/Tools/LeanCode.CodeAnalysis/DiagnosticsIds.cs
+++ src/Tools/LeanCode.CodeAnalysis/DiagnosticsIds.cs
@@ -8,4 +8,7 @@ public static class DiagnosticsIds
     public const string OperationsShouldHaveAuthorizers = "LNCD0004";
     public const string TypedIdMustBeStruct = "LNCD0005";
     public const string CancellationTokensShouldFollowNamingConvention = "LNCD0006";
+    public const string CommandHandlersShouldFollowNamingConvention = "LNCD0007";
+    public const string QueryHandlersShouldFollowNamingConvention = "LNCD0008";
+    public const string OperationHandlersShouldFollowNamingConvention = "LNCD0009";
 }
