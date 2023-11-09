453101fb5ea47a5be380d1647f6f4edc1ab7ce63
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2023-11-09 10:53:33 +0100

    Extend submit app rating with device specific info

--- src/Infrastructure/LeanCode.AppRating.Contracts/SubmitAppRating.cs
+++ src/Infrastructure/LeanCode.AppRating.Contracts/SubmitAppRating.cs
@@ -8,4 +8,14 @@ public class SubmitAppRating : ICommand
 {
     public double Rating { get; set; }
     public string? AdditionalComment { get; set; }
+    public PlatformDTO Platform { get; set; }
+    public string SystemVersion { get; set; }
+    public string AppVersion { get; set; }
+    public Dictionary<string, object>? Metadata { get; set; }
+}
+
+public enum PlatformDTO
+{
+    Android = 0,
+    IOS = 1,
 }
