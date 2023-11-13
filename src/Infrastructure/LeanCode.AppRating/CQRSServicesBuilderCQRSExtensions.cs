941103ea000af46b311e5627f77de42e22a32314
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2023-11-13 16:51:04 +0100

    Accept UserIdExtractor as generic parameter

--- src/Infrastructure/LeanCode.AppRating/CQRSServicesBuilderCQRSExtensions.cs
+++ src/Infrastructure/LeanCode.AppRating/CQRSServicesBuilderCQRSExtensions.cs
@@ -5,21 +5,20 @@ using LeanCode.Components;
 using LeanCode.CQRS.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace LeanCode.AppRating;
 
 public static class CQRSServicesBuilderExtensions
 {
-    public static CQRSServicesBuilder AddAppRating<TUserId, TDbContext>(
-        this CQRSServicesBuilder cqrsServicesBuilder,
-        IUserIdExtractor<TUserId> extractor
+    public static CQRSServicesBuilder AddAppRating<TUserId, TDbContext, TUserIdExtractor>(
+        this CQRSServicesBuilder cqrsServicesBuilder
     )
         where TDbContext : DbContext, IAppRatingStore<TUserId>
         where TUserId : notnull, IEquatable<TUserId>
+        where TUserIdExtractor : class, IUserIdExtractor<TUserId>
     {
         cqrsServicesBuilder.Services.AddTransient<IAppRatingStore<TUserId>>(sp => sp.GetRequiredService<TDbContext>());
-        cqrsServicesBuilder.Services.AddSingleton(extractor);
+        cqrsServicesBuilder.Services.AddSingleton<TUserIdExtractor>();
 
         return cqrsServicesBuilder.AddCQRSObjects(
             TypesCatalog.Of<SubmitAppRating>(),
