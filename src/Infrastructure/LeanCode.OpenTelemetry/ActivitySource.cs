adae481cd29d45bd258ee2716feb1c34436e355a
Jakub Fijałkowski <kuba@codinginfinity.me>
2024-09-11 14:41:35 +0200

    Add methods to start activities by convention

--- src/Infrastructure/LeanCode.OpenTelemetry/ActivitySource.cs
+++ src/Infrastructure/LeanCode.OpenTelemetry/ActivitySource.cs
@@ -7,4 +7,11 @@ public static class LeanCodeActivitySource
     public static readonly ActivitySource ActivitySource = new("LeanCode.CoreLibrary");
 
     public static Activity? Start(string name) => ActivitySource.StartActivity(name);
+
+    public static Activity? StartExecution(string type, string? objectType) => Start($"{type} - {objectType}");
+
+    public static Activity? StartMiddleware(string middlewareName) => Start($"middleware - {middlewareName}");
+
+    public static Activity? StartMiddleware(string middlewareName, string? objectName) =>
+        Start($"middleware - {middlewareName} - {objectName}");
 }
