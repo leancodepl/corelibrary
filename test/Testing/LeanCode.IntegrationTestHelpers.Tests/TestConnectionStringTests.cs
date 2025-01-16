11bec4e99203cd266926ea096f199aa1c0772559
Jakub Fijałkowski <kuba@codinginfinity.me>
2025-01-16 14:24:42 +0100

    Test the new `TestConnectionString` config source

---
+++ test/Testing/LeanCode.IntegrationTestHelpers.Tests/TestConnectionStringTests.cs
@@ -0,0 +1,52 @@
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace LeanCode.IntegrationTestHelpers.Tests;
+
+public class TestConnectionStringTests
+{
+    private const string ConnectionStringKey = "ConnectionString";
+    private const string BaseKey = "Base";
+
+    [Fact]
+    public void Generates_connection_string()
+    {
+        var config = Build("");
+
+        config[ConnectionStringKey].Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Preserves_base_connection_string()
+    {
+        var config = Build("");
+
+        config[BaseKey].Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Final_connection_string_contains_Database_parameter()
+    {
+        var config = Build("");
+
+        config[ConnectionStringKey].Should().Contain("Database=");
+    }
+
+    [Fact]
+    public void Final_connection_string_contains_base_connection_string()
+    {
+        const string BaseConnectionString = "Some=1;Parameter=2";
+        var config = Build(BaseConnectionString);
+
+        config[ConnectionStringKey].Should().Contain(BaseConnectionString);
+    }
+
+    private IConfiguration Build(string baseConnectionString)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.Add(new ConfigurationOverrides(new() { [BaseKey] = baseConnectionString }));
+        builder.Add(new TestConnectionString(BaseKey, ConnectionStringKey));
+        return builder.Build();
+    }
+}
