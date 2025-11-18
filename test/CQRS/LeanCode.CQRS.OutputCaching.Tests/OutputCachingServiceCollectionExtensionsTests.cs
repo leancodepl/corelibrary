043483d73b2ebdbcebb3252bed4fe8b89999e7e9
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-11-18 16:41:29 +0100

    Remove too internal `AddCQRSOtputCache` DI registration test

--- test/CQRS/LeanCode.CQRS.OutputCaching.Tests/OutputCachingServiceCollectionExtensionsTests.cs
+++ test/CQRS/LeanCode.CQRS.OutputCaching.Tests/OutputCachingServiceCollectionExtensionsTests.cs
@@ -1,10 +1,7 @@
 using FluentAssertions;
 using LeanCode.Components;
-using LeanCode.CQRS.Execution;
-using LeanCode.CQRS.OutputCaching.Registration;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace LeanCode.CQRS.OutputCaching.Tests;
@@ -12,28 +9,16 @@ namespace LeanCode.CQRS.OutputCaching.Tests;
 public class OutputCachingServiceCollectionExtensionsTests
 {
     [Fact]
-    public void AddCQRSOutputCache_registers_registry_and_policies()
+    public void AddCQRSOutputCache_registers_proper_caching_policies_from_types_catalog()
     {
         var services = new ServiceCollection();
 
         services.AddCQRSOutputCache(TypesCatalog.Of<TestQueryOCP>());
 
         services
+            .Where(d => d.ServiceType.IsAssignableTo(typeof(IOutputCachePolicy)))
             .Should()
-            .ContainSingle(descriptor =>
-                descriptor.ServiceType == typeof(TestQueryOCP) && descriptor.Lifetime == ServiceLifetime.Transient
-            );
-
-        using var provider = services.BuildServiceProvider();
-        var registry = provider.GetRequiredService<CQRSOutputCacheRegistry>();
-        registry.PolicyForObject.Should().ContainKey(typeof(TestQuery)).WhoseValue.Should().Be<TestQueryOCP>();
-
-        provider.GetRequiredService<ICQRSEndpointMetadataProvider>().Should().BeSameAs(registry);
-
-        provider
-            .GetServices<IConfigureOptions<OutputCacheOptions>>()
-            .OfType<CQRSOutputCacheOptionsConfigurator>()
-            .Should()
-            .ContainSingle();
+            .OnlyContain(d => d.Lifetime == ServiceLifetime.Transient)
+            .And.Satisfy(d => d.ServiceType == typeof(TestQueryOCP), d => d.ServiceType == typeof(TestOperationOCP));
     }
 }
