5553f3df573805104b506dc00e925a6f4a9cee69
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2026-09-14 17:28:32 +0200

    Skip Postgres-only integration tests more automagically

--- test/LeanCode.IntegrationTests/PostgresOnlyFactAttribute.cs
+++ test/LeanCode.IntegrationTests/PostgresOnlyFactAttribute.cs
@@ -3,11 +3,21 @@ using LeanCode.Test.Helpers;
 
 namespace LeanCode.IntegrationTests;
 
-public sealed class PostgresOnlyFactAttribute(
-    [CallerFilePath] string? sourceFilePath = null,
-    [CallerLineNumber] int sourceLineNumber = 0
-) : IntegrationFactAttribute(sourceFilePath, sourceLineNumber)
+public sealed class PostgresOnlyFactAttribute : IntegrationFactAttribute
 {
+    public static bool IsPostgres => Environment.GetEnvironmentVariable(TestDatabaseConfig.ConfigEnvName) == "postgres";
+
+    public PostgresOnlyFactAttribute(
+        [CallerFilePath] string? sourceFilePath = null,
+        [CallerLineNumber] int sourceLineNumber = 0
+    )
+        : base(sourceFilePath, sourceLineNumber)
+    {
+        Skip = "Test requires PostgreSQL.";
+        SkipType = typeof(PostgresOnlyFactAttribute);
+        SkipUnless = nameof(IsPostgres);
+    }
+
     public override IReadOnlyCollection<KeyValuePair<string, string>> GetTraits() =>
         base.GetTraits().Append(new("database", "postgres")).ToList();
 }
